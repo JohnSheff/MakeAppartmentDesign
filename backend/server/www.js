98c029dde0fe9474ad076ef3d34bdd926cad67d7
Evgeniy <mdt-pro@mail.ru>
2020-02-03 16:46:04 +0300

    deploy heroku

--- backend/server/www.js
+++ backend/server/www.js
@@ -7,7 +7,7 @@ const Filestore = require ('session-file-store') (session);
 const indexRouter = require ('../routes/index');
 const bodyParser = require ('body-parser');
 require('dotenv').config()
-
+const path = require('path');
 const mongoose = require ('mongoose');
 const app = express ();
 mongoose.connect (`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@clusterelbrus-ntmbz.mongodb.net/makeApp?retryWrites=true&w=majority`, {
@@ -16,10 +16,17 @@ mongoose.connect (`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@c
   useFindAndModify: false
 });
 
+const publicPath = path.join(__dirname, '..', 'build');
+app.use(express.static(publicPath))
+
 http.createServer (app).listen (process.env.PORT || 5010)
 app.use (logger ('dev'));
 // const port = '5010';
 // app.set ('port', "5010");
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(publicPath, 'index.html'));
+});
 
 app.use (session ({
   store: new Filestore (), key: 'user_sid', secret: 'anything', resave: false, saveUninitialized: false, cookie: {
